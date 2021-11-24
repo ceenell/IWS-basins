ef70f3a9cd4117695dca77ee65d9e086df183c50
collnell <cnell@usgs.gov>
2021-11-24 10:09:17 -0600

    restructure layout into components

--- src/views/Visualization.vue
+++ src/views/Visualization.vue
@@ -2,13 +2,16 @@
   <div id="visualization">
     <div class="wrapper">
       <div id="title-container">
-      <h1>Addressing water resource challenges</h1>
-      <p>The U.S. Geological Survey is addressing the Nation’s greatest water resource challenges through the intensive study of 10 Integrated Water Science (IWS) basins. The IWS basins will represent a wide range of environmental, hydrologic, and landscape settings and human stressors of water resources to improve understanding of water availability.</p>
+        <h1>Addressing water resource challenges</h1>
+        <p>The U.S. Geological Survey is addressing the Nation’s greatest water resource challenges through the intensive study of 10 Integrated Water Science (IWS) basins. The IWS basins will represent a wide range of environmental, hydrologic, and landscape settings and human stressors of water resources to improve understanding of water availability.</p>
       </div>
-    <article class="content">
-      <CONUS />
-    </article>
-  </div>
+      <div id="conus-container">
+        <CONUS />
+      </div>
+      <div id="huc-container">
+        <HUC />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,7 +20,8 @@
 export default {
     name: 'Visualization',
     components: {
-      CONUS: () => import( /* webpackPreload: true */ /*webpackChunkName: "section"*/ "./../components/CONUS")
+      CONUS: () => import( /* webpackPreload: true */ /*webpackChunkName: "section"*/ "./../components/CONUS"),
+      HUC: () => import( /* webpackPreload: true */ /*webpackChunkName: "section"*/ "./../components/HUC")
     },
     computed: {
     },
@@ -34,39 +38,42 @@ $text_color: white;
 $bkgd_color: rgb(41, 41, 41);
 $hilite: rgb(208, 138, 223);
 
-// map style
-.usa {
-  fill: transparent;
-  stroke:white;
-}
-.state {
-  stroke: white;
-  fill: transparent;
-}
-
 // layout
 #visualization {
   margin: 4rem;
 }
-.content {
-  grid-area: content;
-  background-color: $bkgd_color;
-}
-
 .title-container {
   grid-area: title;
 }
+#conus-container {
+  grid-area: CONUS;
+}
+#huc-container {
+  grid-area: HUC;
+}
+// mobile layout
 .wrapper {
   display: grid;
   gap: 20px;
   grid-template-areas:
-    "title title"
-    "content content";
+    "title"
+    "CONUS"
+    "HUC"
+    "chart";
 }
 // desktop
 @media (min-width: 900px) {
   #visualization {
   margin: 5rem;
-}
+  }
+  .wrapper {
+    display: grid;
+    gap: 20px;
+    grid-template-columns: 3fr 2fr;
+    grid-template-areas:
+      "title title"
+      "CONUS HUC"
+      "chart chart";
+  }
 }
 </style>
